d0b5d87713d760042f0223fd48e5b01fdf142257
harshtalks <harshpareek91@gmail.com>
2021-08-10 13:53:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 close = document.querySelector('.close')
 open = document.querySelector('.mainStuff');
 
-close.addEventListener('click', function(){
-    document.querySelector('.hover--box').classList.add('displayBox')
-    document.querySelector('.container').classList.remove('displayBox')
-    document.querySelector('.container').style.display = 'block'
-})
 
-open.addEventListener('click',function(){
-    document.querySelector('.hover--box').classList.remove('displayBox')
-    document.querySelector('.container').classList.add('displayBox')
-    document.querySelector('.container').style.display = 'none'
-})
+
+ 
+setTimeout(function(){
+        document.querySelector('.container').classList.remove('preloader-display')
+        document.querySelector('.preloader').classList.add('preloader-display')
+    }, 3000);
+ 
+
